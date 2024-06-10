fca0dfd15542d60df7b8172dcdfcc089d9ae575d
Jesray Garciano <jesraygarciano@Jesrays-MacBook-Pro.local>
2024-06-10 23:33:13 +0800

    Initialized LandMark model

---
+++ app/dist/types.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.SupportedModels = void 0;
+var SupportedModels;
+(function (SupportedModels) {
+    SupportedModels["MediaPipeFaceMesh"] = "MediaPipeFaceMesh";
+})(SupportedModels = exports.SupportedModels || (exports.SupportedModels = {}));
